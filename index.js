d84bcdad24aeb67ed36b8b392e756a3d2bf381bf
joseph <josephmacharia575@gmail.com>
2022-08-29 21:24:21 +0300

    DONE

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+	let message = [];
+	for (let a = 0; a < names.length; a++) {
+		message.push(`Thank you, ${names[a]}, for the wonderful ${event} gift!`);
+	}
+	return message;
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "Surprise"));
+function countDown(number) {
+	while (number >= 0) {
+		console.log(number);
+		number--;
+	}
+}
